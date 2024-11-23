575ba75fdd4ed7bbb99a6c1ee4a2fe5f64917a15
bingo <165366261+Danya-Omelchenko@users.noreply.github.com>
2024-11-23 09:26:57 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,69 @@ window.addEventListener('resize', function(){
         menuToggle.classList.remove("active");
     }
 });
+//Мобильная версия
+const slider = document.querySelector('.slide');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+const slides = Array.from(slider.querySelectorAll('li'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'flex';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+updateSlider();
+
+//Стандартная версия
+let listImgslide = document.querySelectorAll('.avtov');
+const btnRight = document.querySelector('.next-button');
+const sliderBlock = document.querySelector('.slider');
+const btnLeft = document.querySelector('.prev-button');
+
+btnRight.addEventListener('click', () =>{
+    listImgslide = document.querySelectorAll('.avtov');
+    console.log();
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = listImgslide[0].children[0].src;
+    img.classList.add('avto');
+    li.classList.add('avtov');
+    li.appendChild(img);
+    sliderBlock.removeChild(listImgslide[0]);
+    sliderBlock.appendChild(li);
+})
+btnLeft.addEventListener('click', () =>{
+    listImgslide = document.querySelectorAll('.avtov');
+    console.log();
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    img.src = listImgslide[0].children[0].src;
+    img.classList.add('avto');
+    li.classList.add('avtov');
+    li.appendChild(img);
+    sliderBlock.removeChild(listImgslide[0]);
+    sliderBlock.appendChild(li);
+})
